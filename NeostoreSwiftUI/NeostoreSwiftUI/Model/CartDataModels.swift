62f8cd665a4e68e8738997300bb95b6620bb067d
rashidakapadia2 <rashidakapadia2@gmail.com>
2023-05-30 18:20:46 +0530

    model

--- NeostoreSwiftUI/NeostoreSwiftUI/Model/CartDataModels.swift
+++ NeostoreSwiftUI/NeostoreSwiftUI/Model/CartDataModels.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct RegisterModel: Hashable, Codable, Identifiable {
+    let id: UUID
+    let first_name: String?
+    let last_name: String?
+    let email: String?
+    let password: String?
+    let confirm_password: String?
+    let gender: String?
+    let phone_no: String?
+}
+
+struct LoginModel: Hashable, Codable, Identifiable {
+    let id: UUID
+    let status: Bool
+    let data: LoginResponseModel?
+    let message: String?
+    let user_msg: String?
+}
+
+struct LoginResponseModel: Hashable, Codable, Identifiable {
+    let id: UUID
+    let email: String
+    let password: String
+}
+
+struct GenericResponseModel: Hashable, Codable, Identifiable {
+    let id: UUID
+    let status: Bool
+    let message: String?
+    let user_msg: String?
+}
